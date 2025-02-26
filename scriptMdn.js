8cfb134a09f4fcbbe6bed86ce6444948c0c83a79
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-02-26 16:13:20 -0700

    Practice

--- scriptMdn.js
+++ scriptMdn.js
@@ -4,3 +4,26 @@ link.textContent = "Mozilla Developer Network"
 link.href = "https://developer.mozilla.org"
 
 const sect = document.querySelector("section")
+const para = document.createElement("p")
+para.textContent = "We hope you enjoyed the ride."
+sect.appendChild(para)
+const text = document.createTextNode(
+  " - the premier source for web development knowledge."
+)
+const linkPara = document.querySelector("p")
+linkPara.appendChild(text)
+
+
+const linkPara2 = linkPara.cloneNode(true)
+sect.appendChild(linkPara2)
+
+// sect.removeChild(linkPara2)
+linkPara2.remove()
+
+// console.log(document.styleSheets)
+// para.style.color = "white"
+// para.style.backgroundColor = "black"
+// para.style.padding = "10px"
+// para.style.width = "250px"
+// para.style.textAlign = "center"
+para.setAttribute("class", "highlight")
